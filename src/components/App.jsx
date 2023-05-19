f07317d56a8d487c7db5fbeef2ce49e580ddf92d
Michał Jasiński <jasinskimichal.1996@gmail.com>
2023-05-19 13:46:43 +0200

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { nanoid } from 'nanoid';
+import {
+  addContact,
+  deleteContact,
+  setFilter,
+  initializeContacts,
+} from '../redux/contactSlice';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import styles from './app.module.css';
+
+function App() {
+  const filter = useSelector(state => state.contacts.filter);
+  const items = useSelector(state => state.contacts.items);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(initializeContacts());
+  }, [dispatch]);
+
+  const handleAddContact = (name, number) => {
+    const id = nanoid();
+    dispatch(addContact({ id, name, number }));
+
+    const updatedContacts = [...items, { id, name, number }];
+    localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+  };
+
+  const handleFilterChange = e => {
+    const { value } = e.target;
+    dispatch(setFilter(value));
+  };
+
+  const filteredContacts = items.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  const handleDeleteContact = contactId => {
+    dispatch(deleteContact(contactId));
+
+    const updatedContacts = items.filter(contact => contact.id !== contactId);
+    localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styles.container}>
+      <div className={styles.phonebook}>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={handleAddContact} />
+      </div>
+
+      <div className={styles.contacts}>
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={handleFilterChange} />
+        <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact} />
+      </div>
     </div>
   );
-};
+}
+
+export default App;
